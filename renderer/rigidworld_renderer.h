53d77477c7b0269465b105e096f3a356c3f3a998
OverflowEx <liyao_0419@outlook.com>
2026-02-05 22:36:52 +0800

    expand color palette

--- renderer/rigidworld_renderer.h
+++ renderer/rigidworld_renderer.h
@@ -123,7 +123,7 @@ private:
 
 	std::map<Shape, Model> _phong_models; // material type: phong lighitng model + shadow
 
-	const std::vector<Color> _palette = { RED, GREEN, BLUE, YELLOW, PURPLE, SKYBLUE };
+	const std::vector<Color> _palette = { RED, GREEN, BLUE, YELLOW, PURPLE, SKYBLUE, BEIGE };
 
 	struct Body {
 		// Shape shape;
